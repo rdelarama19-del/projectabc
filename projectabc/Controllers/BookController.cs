61b84548120d26e5d7ed9b2e627bb5aaba11dd27
rdelarama19 <rdelarama19@gmail.com>
2026-04-16 17:47:17 +0800

    20260416 3

---
+++ projectabc/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace projectabc.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ projectabc/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace projectabc.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Category { get; set; }
+        public int PublishedYear { get; set; }
+        public bool IsAvailable { get; set; }
+        public string Image { get; set; }
+
+    }
+}
